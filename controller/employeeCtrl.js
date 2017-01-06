0526c490b28124cbae56d3459f7ff8ad3a98d38c
shilpareddy123 <shilpareddy.2793@gmail.com>
2017-01-06 18:44:13 +0530

    sending reuest to server

--- controller/employeeCtrl.js
+++ controller/employeeCtrl.js
@@ -5,47 +5,66 @@
 angular.module('mainApp')
 .controller('employeeCtrl', employeeCtrl);
 
-/* Controllers */
-function employeeCtrl($scope) {
-  $scope.today = new Date();
-    $scope.items = [{
-        employeeName:'Swati',
-        employeeStatus: 'Fellowship',
-        company: 'BridgeLabz',
-        mobile:'9876000012',
-        emailId : 'artipatel@gmail.com',
-        src: 'images/IMG_20161218_184613_1482391834595(1).jpg'
-      },
-      {
-          employeeName:'Pranali',
-          employeeStatus: 'Fellowship',
-          company: 'BridgeLabz',
-          mobile:'9876000012',
-          emailId : 'artipatel@gmail.com',
-          src: 'images/images(1).jpg'
-        },{
-            employeeName:'Virat',
-            employeeStatus: 'Fellowship',
-            company: 'BridgeLabz',
-            mobile:'9876000012',
-            emailId : 'artipatel@gmail.com',
-            src: 'images/kohli2503.jpg'
-          },  {
-                employeeName:'xyz',
-                employeeStatus: 'Fellowship',
-                company: 'BridgeLabz',
-                mobile:'9876000012',
-                emailId : 'artipatel@gmail.com',
-                src: 'images/image.004.jpg'
-              },{
-                  employeeName:'abc',
-                  employeeStatus: 'Fellowship',
-                  company: 'BridgeLabz',
-                  mobile:'9876000012',
-                  emailId : 'artipatel@gmail.com',
-                  src: 'images/index.000.jpg'
-                }];
+function employeeCtrl($scope,$http) {
+console.log("unmark");
+  var akey=localStorage.getItem('satellizer_token');
+$scope.today=new Date();
 
+  $http({
+    "method":"POST",
+    "url":"http://192.168.0.171:3000/sendEmailToUnmarkedEmployee",
+    "data":{token:"fjhdfdjkfdkfdkbfk",timeStamp:Date.now()}
+  }).then(function(data){
+    console.log(data.data);
+
+    $scope.items=data.data.unmarkEmployee;
+    
+  }).catch(function(err){
+    console.log(err);
+  })
+
+
+/* Controllers */
+// function employeeCtrl($scope) {
+//   $scope.today = new Date();
+//     $scope.items = [{
+//         employeeName:'Swati',
+//         employeeStatus: 'Fellowship',
+//         company: 'BridgeLabz',
+//         mobile:'9876000012',
+//         emailId : 'artipatel@gmail.com',
+//         src: 'images/IMG_20161218_184613_1482391834595(1).jpg'
+//       },
+//       {
+//           employeeName:'Pranali',
+//           employeeStatus: 'Fellowship',
+//           company: 'BridgeLabz',
+//           mobile:'9876000012',
+//           emailId : 'artipatel@gmail.com',
+//           src: 'images/images(1).jpg'
+//         },{
+//             employeeName:'Virat',
+//             employeeStatus: 'Fellowship',
+//             company: 'BridgeLabz',
+//             mobile:'9876000012',
+//             emailId : 'artipatel@gmail.com',
+//             src: 'images/kohli2503.jpg'
+//           },  {
+//                 employeeName:'xyz',
+//                 employeeStatus: 'Fellowship',
+//                 company: 'BridgeLabz',
+//                 mobile:'9876000012',
+//                 emailId : 'artipatel@gmail.com',
+//                 src: 'images/image.004.jpg'
+//               },{
+//                   employeeName:'abc',
+//                   employeeStatus: 'Fellowship',
+//                   company: 'BridgeLabz',
+//                   mobile:'9876000012',
+//                   emailId : 'artipatel@gmail.com',
+//                   src: 'images/index.000.jpg'
+//                 }];
+//
     $scope.cardItems = [];
 
     $scope.employees = function (employeeName, employeeStatus,company,mobile,emailId) {
@@ -60,20 +79,6 @@ function employeeCtrl($scope) {
     };
 
 }
-angular.module('mainApp').controller('employeeCtrl', function($scope,$http) {
-// localStorage.setItem('key', value);
-var akey=localStorage.getItem('satellizer_token');
- console.log("key:",akey);
-
-$http({
-method: 'GET',
-url: 'http://192.168.0.171:3000//sendEmailToUnmarkedEmployee?timeStamp=1483315200000&token=21hghj'
-
-}).then(function(data) {
-console.log(data);
-
-});
-});
 
 /* Directives */
 
--- controller/localCtrl.js
+++ controller/localCtrl.js
@@ -7,3 +7,123 @@ angular.module('mainApp').controller('localCtrl',function(myLocalStorage){
    return myLocalStorage.setItem(val);
  };
 });
+
+
+angular.module('mainApp')
+.controller('empCtrl', empCtrl);
+/* Controllers */
+function empCtrl($scope,$http) {
+  console.log("fallout");
+  $scope.today = new Date();
+  $http({"url":"http://192.168.0.171:3000/readFalloutAttendanceEmployee?token=f12sd1fd2sf1&timeStamp="+Date.now(),
+"method":"GET"}).then(function(data){
+  console.log(data.data);
+  $scope.items= data.data.falloutEmployee;
+  }).catch(function(err){
+    console.log(err);
+  })
+// $scope.items = [{
+//         employeeName:'Naresh shanghvi',
+//         employeeStatus: 'Internship',
+//         company: 'BridgeLabz',
+//         mobile:'1234567890',
+//         emailId :'artichabra@bridgelabz.com',
+//         src: 'images/pppp profilepic.jpg'
+//       },
+//       {
+//           employeeName:'Sohail taanveer',
+//           employeeStatus: 'Fellowship',
+//           company: 'BridgeLabz',
+//           mobile:'1234567890',
+//           emailId :'arti2793@bridgelabz.com',
+//           src: 'images/431.png'
+//         },{
+//             employeeName:'Amit arunthee',
+//             employeeStatus: 'Fellowship',
+//             company: 'BridgeLabz',
+//             mobile:'1234567890',
+//             emailId :'sushantsingh@bridgelabz.com',
+//             src: 'images/virat_kohli.png'
+//           },  {
+//                 employeeName:'Durga Mahtab',
+//                 employeeStatus: 'Fellowship',
+//                 company: 'BridgeLabz',
+//                 mobile:'1234567890',
+//                 emailId :'pp@bridgelabz.com',
+//                 src: 'images/ab_de_villiers.png'
+//               },{
+//                   employeeName:'Hamid Raza Noori',
+//                   employeeStatus: 'Fellowship',
+//                   company: 'BridgeLabz',
+//                   mobile:'1234567890',
+//                   emailId :'pp@bridgelabz.com',
+//                   src: 'images/IMG_20160217_125454.jpg'
+//                 },{
+//                       employeeName:'Shalini Reddy',
+//                       employeeStatus: 'Internship',
+//                       company: 'BridgeLabz',
+//                       mobile:'1234567890',
+//                       emailId :'pp@bridgelabz.com',
+//                       src: 'images/Screenshot from 2016-10-22 09:30:27.png'
+//                     },
+//                     {
+//                         employeeName:'Sharad bhai pawar',
+//                         employeeStatus: 'Fellowship',
+//                         company: 'BridgeLabz',
+//                         mobile:'1234567890',
+//                         emailId :'pp@bridgelabz.com',
+//                         src: 'images/pppp profilepic.jpg'
+//                       },{
+//                           employeeName:'Swati Dindule',
+//                           employeeStatus: 'Fellowship',
+//                           company: 'BridgeLabz',
+//                           mobile:'1234567890',
+//                           emailId :'pp@bridgelabz.com',
+//                           src: 'images/_20161230_100546.JPG'
+//                         }];
+
+
+
+
+    $scope.cardItems = [];
+
+    $scope.employees = function (employeeName, employeeStatus,company,mobile,emailId) {
+        var objAdded = {
+            employeeName:employeeName,
+            employeeStatus:employeeStatus,
+            company:company,
+            mobile:mobile,
+            emailId:emailId
+        };
+        $scope.cardItems.push(objAdded);
+    };
+
+}
+/* Directives */
+
+angular.module('mainApp')
+
+.directive('itemCard', function () {
+    // return the directive definition object
+    return {
+        scope: {
+            item:"="
+        },
+        controller: function ($scope, $element, $attrs, $location) {
+            $scope.addToCart = function (value, key) {
+                var mainScope = angular.element("#main").scope();
+                mainScope.employees(value, key);
+                return false;
+            };
+        },
+        replace: true,
+        template: '<div class="item">\
+        <div class="item-int"><h4>{{item.employeeName}}</h4>\
+                <div class="data"><img ng-src="{{item.src}}"/>\
+                <table>\
+              <tr><td>{{item.employeeStatus}}</td><tr>\
+            <tr><td>{{item.company}}</td><tr>\
+            <tr><td>{{item.mobile}}</td><tr>\
+              <tr><td>{{item.emailId}}</td></tr></table></div></div></div>'
+    };
+});
